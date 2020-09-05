35926a6fdd4c4526f74b063d6c5193b76787b42f
ofir <gennin@gmail.com>
2020-09-05 14:24:51 +0300

    Fixed bugs - keyboard overflow
    moved login/logout form to the top of the screen
    keyboard now dont overload text field

--- quiz_game/lib/views/signIn.dart
+++ quiz_game/lib/views/signIn.dart
@@ -58,100 +58,102 @@ class _SignInState extends State<SignIn> {
       ),
       body:  isLoading ? Loading() : Form(
         key: _formKey,
-        child: Container(
-          margin: EdgeInsets.symmetric(horizontal: margin),
-          child: Column(
-            children: [
-              Spacer(),
-              TextFormField(
-                validator: (val) {
-                  return val.isEmpty ? "Email cannot be empty" : null;
-                },
-                decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                onChanged: (val){
-                  email = val;
-                },
-              ),//email
-              SizedBox(height: 6,),
-              TextFormField(
-                obscureText: true,
-                validator: (val) {
-                  return val.isEmpty ? "Password cannot be empty" : null;
-                },
-                decoration: textInputDecoration.copyWith(hintText: 'Password'),
-                onChanged: (val){
-                  password = val;
-                },
-              ),//password
+        child: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: margin),
+            child: Column(
+              children: [
+               //Spacer(),
+                TextFormField(
+                  validator: (val) {
+                    return val.isEmpty ? "Email cannot be empty" : null;
+                  },
+                  decoration: textInputDecoration.copyWith(hintText: 'Email'),
+                  onChanged: (val){
+                    email = val;
+                  },
+                ),//email
+                SizedBox(height: 6,),
+                TextFormField(
+                  obscureText: true,
+                  validator: (val) {
+                    return val.isEmpty ? "Password cannot be empty" : null;
+                  },
+                  decoration: textInputDecoration.copyWith(hintText: 'Password'),
+                  onChanged: (val){
+                    password = val;
+                  },
+                ),//password
 
-              SizedBox(height: 24,),
+                SizedBox(height: 24,),
 
 
-              GestureDetector(
-                onTap: () async {
-                  await signIn();
-                },
-                child: Container(
-                  padding: EdgeInsets.symmetric(vertical: 18),
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  alignment: Alignment.center,
-                  width: MediaQuery.of(context).size.width - margin * 2,
-                  child: Text(
-                    'Sign in',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
+                GestureDetector(
+                  onTap: () async {
+                    await signIn();
+                  },
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 18),
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(30),
                     ),
-                  ),
-                ),
-              ),//sign in btn container
-              SizedBox(height: 18,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Don't have an account? ",
-                    style: TextStyle(
-                      fontSize: 15.5,
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushReplacement(
-                          context
-                          , MaterialPageRoute(
-                          builder: (context) => SignUp()
-                      )
-                      );
-                    },
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width - margin * 2,
                     child: Text(
-                      "Sign Up ",
+                      'Sign in',
                       style: TextStyle(
-                        fontSize: 15.5,
-                        decoration: TextDecoration.underline,
+                        color: Colors.white,
+                        fontSize: 16,
                       ),
                     ),
                   ),
+                ),//sign in btn container
+                SizedBox(height: 18,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account? ",
+                      style: TextStyle(
+                        fontSize: 15.5,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushReplacement(
+                            context
+                            , MaterialPageRoute(
+                            builder: (context) => SignUp()
+                        )
+                        );
+                      },
+                      child: Text(
+                        "Sign Up ",
+                        style: TextStyle(
+                          fontSize: 15.5,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
 
 
 
-                ],
-              ),//sign up
+                  ],
+                ),//sign up
 
-              SizedBox(height: 12,),
-              Text(
-                error,
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 14,
+                SizedBox(height: 12,),
+                Text(
+                  error,
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 14,
+                  ),
                 ),
-              ),
 
-              SizedBox(height: 80,),
-            ],
+                SizedBox(height: 80,),
+              ],
+            ),
           ),
         ),
       ),
--- quiz_game/lib/views/signUp.dart
+++ quiz_game/lib/views/signUp.dart
@@ -55,108 +55,110 @@ class _SignUpState extends State<SignUp> {
       ),
       body: isLoading ? Loading() : Form(
         key: _formKey,
-        child: Container(
-          margin: EdgeInsets.symmetric(horizontal: margin),
-          child: Column(
-            children: [
-              Spacer(),
-              TextFormField(
-                validator: (val) {
-                  return val.isEmpty ? "Name cannot be empty" : null;
-                },
-                decoration: textInputDecoration.copyWith(hintText: 'Name'),
-                onChanged: (val){
-                  name = val;
-                },
-              ),//Name
-              TextFormField(
-                validator: (val) {
-                  return val.isEmpty ? "Email cannot be empty" : null;
-                },
-                decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                onChanged: (val){
-                  email = val;
-                },
-              ),//email
-              SizedBox(height: 6,),
-              TextFormField(
-                obscureText: true,
-                validator: (val) {
-                  return val.isEmpty ? "Password cannot be empty" : null;
-                },
-                decoration: textInputDecoration.copyWith(hintText: 'Password'),
-                onChanged: (val){
-                  password = val;
-                },
-              ),//password
+        child: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: margin),
+            child: Column(
+              children: [
+                //Spacer(),
+                TextFormField(
+                  validator: (val) {
+                    return val.isEmpty ? "Name cannot be empty" : null;
+                  },
+                  decoration: textInputDecoration.copyWith(hintText: 'Name'),
+                  onChanged: (val){
+                    name = val;
+                  },
+                ),//Name
+                TextFormField(
+                  validator: (val) {
+                    return val.isEmpty ? "Email cannot be empty" : null;
+                  },
+                  decoration: textInputDecoration.copyWith(hintText: 'Email'),
+                  onChanged: (val){
+                    email = val;
+                  },
+                ),//email
+                SizedBox(height: 6,),
+                TextFormField(
+                  obscureText: true,
+                  validator: (val) {
+                    return val.isEmpty ? "Password cannot be empty" : null;
+                  },
+                  decoration: textInputDecoration.copyWith(hintText: 'Password'),
+                  onChanged: (val){
+                    password = val;
+                  },
+                ),//password
 
-              SizedBox(height: 24,),
-              GestureDetector(
-                onTap: () {
-                  signUp();
-                },//btn sign up logic
-                child: Container(
-                  padding: EdgeInsets.symmetric(vertical: 18),
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  alignment: Alignment.center,
-                  width: MediaQuery.of(context).size.width - margin * 2,
-                  child: Text(
-                    'Sign up',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
+                SizedBox(height: 24,),
+                GestureDetector(
+                  onTap: () {
+                    signUp();
+                  },//btn sign up logic
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 18),
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(30),
                     ),
-                  ),
-                ),
-              ),//sign in btn container
-              SizedBox(height: 18,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Already have an account? ",
-                    style: TextStyle(
-                      fontSize: 15.5,
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushReplacement(
-                          context
-                          , MaterialPageRoute(
-                          builder: (context) => SignIn()
-                        )
-                      );
-                    },
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width - margin * 2,
                     child: Text(
-                      "Sign In ",
+                      'Sign up',
                       style: TextStyle(
-                        fontSize: 15.5,
-                        decoration: TextDecoration.underline,
+                        color: Colors.white,
+                        fontSize: 16,
                       ),
                     ),
                   ),
+                ),//sign in btn container
+                SizedBox(height: 18,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Already have an account? ",
+                      style: TextStyle(
+                        fontSize: 15.5,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushReplacement(
+                            context
+                            , MaterialPageRoute(
+                            builder: (context) => SignIn()
+                          )
+                        );
+                      },
+                      child: Text(
+                        "Sign In ",
+                        style: TextStyle(
+                          fontSize: 15.5,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
 
 
 
-                ],
-              ),//sign up
+                  ],
+                ),//sign up
 
 
-              SizedBox(height: 12,),
-              Text(
-                error,
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 14,
+                SizedBox(height: 12,),
+                Text(
+                  error,
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 14,
+                  ),
                 ),
-              ),
 
-              SizedBox(height: 80,),
-            ],
+                SizedBox(height: 80,),
+              ],
+            ),
           ),
         ),
       ),
